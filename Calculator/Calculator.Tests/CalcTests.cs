31750c6034c292b2c9be18f813a7d0caf333f01e
André <AndreR@ppedv.de>
2024-10-14 12:32:49 +0200

    Add unit tests, .lutignore, and Live Unit Testing config
    Updated CalcTests.cs:
    - Added IDisposable implementation and Dispose method.
    - Included additional unit tests with [Fact] and [Theory].
    - Added using System.ComponentModel for attributes.
    
    Added .lutignore:
    - Configured to ignore specific files and directories for Live Unit Testing.
    
    Added Calculator.lutconfig:
    - Enabled parallel builds and test runs.
    - Set test case timeout to 180,000 ms.

--- Calculator/Calculator.Tests/CalcTests.cs
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -1,8 +1,12 @@
-﻿namespace Calculator.Tests
+﻿using System.ComponentModel;
+
+namespace Calculator.Tests
 {
-    public class CalcTests
+    public class CalcTests : IDisposable
     {
+
         [Fact]
+        [Trait("Category", "Unit")]
         public void Sum_2_and_3_results_5()
         {
             //Arrage
@@ -35,6 +39,35 @@
             var calc = new Calc();
 
             Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, -2, -1)]
+        [InlineData(0, 0, 0)]
+        [InlineData(5000, 2000, 7000)]
+        public void Sum_with_result(int a, int b, int exp)
+        {
+            var calc = new Calc();
+
+            var result = calc.Sum(a, b);
+
+            Assert.Equal(exp, result);
+        }
+
+        [Theory]
+        [InlineData(int.MaxValue, 1)]
+        [InlineData(int.MinValue, -1)]
+        public void Sum_throws_OverflowException(int a, int b)
+        {
+            var calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(a, b));
+        }
+
+        public void Dispose()
+        {
+
         }
     }
 }
