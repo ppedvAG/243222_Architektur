770e6acaeaedf0e494d046ec4b118cc09c93a6e1
André <AndreR@ppedv.de>
2024-10-15 10:07:44 +0200

    Add initial project setup with Schrank builder pattern
    Introduce a new Visual Studio solution file (`HalloBuilder.sln`) and a project file (`HalloBuilder.csproj`) targeting .NET 8.0. Add `Program.cs` to demonstrate the builder pattern for creating `Schrank` objects. Define `Schrank` class with properties and a nested `Builder` class, including validation logic and an `Oberfläche` enum.

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,8 @@
+﻿using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+
+var schrank = new Schrank.Builder().SetFarbe("Rot").SetAnzTüren(333).SetAnzBöden(5554).SetOberfläche(Oberfläche.Gewachst).Build();
+
+Console.WriteLine("ende");
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,64 @@
+﻿namespace HalloBuilder
+{
+    public class Schrank
+    {
+        public int AnzTüren { get; private set; }
+        public int AnzBöden { get; private set; }
+        public string Farbe { get; private set; } = string.Empty;
+        public Oberfläche Oberfläche { get; private set; }
+
+        private Schrank()
+        { }
+
+        public class Builder
+        {
+            Schrank _schrank = new Schrank();
+
+            public Builder SetAnzTüren(int türen)
+            {
+                if (türen < 2 && türen > 7)
+                    throw new System.ArgumentException("Anzahl der Türen muss zwischen 2 und 7 liegen");
+
+                _schrank.AnzTüren = türen;
+                return this;
+            }
+
+            public Builder SetAnzBöden(int böden)
+            {
+                if (böden < 1 || böden > 6)
+                    throw new System.ArgumentException("Anzahl der Böden muss zwischen 1 und 6 liegen");
+
+                _schrank.AnzBöden = böden;
+                return this;
+            }
+
+            public Builder SetFarbe(string farbe)
+            {
+                if (_schrank.Oberfläche != Oberfläche.Lackiert)
+                    throw new System.ArgumentException("Farbe kann nur Lackiert");
+
+                _schrank.Farbe = farbe;
+                return this;
+            }
+
+            public Builder SetOberfläche(Oberfläche oberfläche)
+            {
+                _schrank.Oberfläche = oberfläche;
+                return this;
+            }
+
+            public Schrank Build()
+            {
+                return _schrank;
+            }
+        }
+
+    }
+
+    public enum Oberfläche
+    {
+        Natur,
+        Gewachst,
+        Lackiert
+    }
+}
