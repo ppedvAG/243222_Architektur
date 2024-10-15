b483abd1602e03780480567190021adbc951647f
André <AndreR@ppedv.de>
2024-10-15 14:40:50 +0200

    Add new Db project and EF Core context
    Added a new project `BeanRider.Data.Db` to the solution, configured with .NET 8.0 and necessary package references. Introduced `EfContext` class in `EfContext.cs` for Entity Framework Core, including `DbSet` properties for various entities and SQL Server configuration with lazy loading proxies.

---
+++ BeanRider/BeanRider.Data.Db/EfContext.cs
@@ -0,0 +1,28 @@
+﻿using BeanRider.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeanRider.Data.Db
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Eatable> Eatables { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        string _conString;
+
+        public EfContext(string conString)
+        {
+            _conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_conString).UseLazyLoadingProxies();
+        }
+
+    }
+}
