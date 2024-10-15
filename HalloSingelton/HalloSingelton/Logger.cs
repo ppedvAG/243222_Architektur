d3a9a4cd922d746d028f896dbf12c52e5b8c29b2
André <AndreR@ppedv.de>
2024-10-15 09:42:55 +0200

    Add new solutions for Factory and Singleton patterns
    Introduce two new Visual Studio solution files: `HalloFactory.sln` and `HalloSingelton.sln`.
    
    In `HalloFactory.sln`:
    - Add `HalloFactory.csproj` for .NET 7.0 with SQL client packages.
    - Implement `Program.cs` to connect to SQL Server or SQLite and count employees.
    
    In `HalloSingelton.sln`:
    - Add `HalloSingelton.csproj` for .NET 7.0.
    - Implement `Logger.cs` with thread-safe singleton logger.
    - Demonstrate logger usage in `Program.cs` with multiple tasks.

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,36 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static Logger instance;
+        private static object sync = new object();
+        public static Logger Instance
+        {
+            get
+            {
+                lock (sync)
+                {
+                    if (instance == null)
+                        instance = new Logger();
+                }
+
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            Info("Neuer Logger");
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine($"{DateTime.Now:g} [INFO] {message}");
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"{DateTime.Now:g} [ERROR] {message}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,12 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 10; i++)
+    Task.Run(() => Logger.Instance.Info($"Hallo {i}"));
+
+Logger.Instance.Info("App started");
+
+Logger.Instance.Info("App is working");
+Logger.Instance.Error("App crashed");
+
