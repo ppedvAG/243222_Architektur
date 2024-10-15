8ef89751c40f70f0d667a0890f198814883c42f4
André <AndreR@ppedv.de>
2024-10-15 11:01:26 +0200

    Add HalloDecorator project with decorator pattern example
    Added a new Visual Studio solution `HalloDecorator.sln` with configurations for Debug and Release builds. Introduced `Deko.cs` defining the `ICompoment` interface and classes `Pizza`, `Brot`, `Deko`, `Salami`, and `Käse` to demonstrate the decorator pattern. Created `HalloDecorator.csproj` targeting .NET 8.0. Added `Program.cs` with a console app showcasing decorated components and JSON serialization.

---
+++ HalloDecorator/HalloDecorator/Deko.cs
@@ -0,0 +1,49 @@
+﻿
+interface ICompoment
+{
+    string Name { get; }
+    decimal Preis { get; }
+}
+
+class Pizza : ICompoment
+{
+    public string Name => "Pizza";
+
+    public decimal Preis => 5m;
+}
+
+class Brot : ICompoment
+{
+    public string Name => "Brot";
+
+    public decimal Preis => 2m;
+}
+
+abstract class Deko : ICompoment
+{
+    protected ICompoment _compoment;
+    public Deko(ICompoment compoment)
+    {
+        _compoment = compoment;
+    }
+    public abstract string Name { get; }
+    public abstract decimal Preis { get; }
+}
+
+class Salami : Deko
+{
+    public Salami(ICompoment compoment) : base(compoment)
+    {
+    }
+    public override string Name => _compoment.Name + " mit Salami";
+    public override decimal Preis => _compoment.Preis + 3m;
+}
+
+class Käse : Deko
+{
+    public Käse(ICompoment compoment) : base(compoment)
+    {
+    }
+    public override string Name => _compoment.Name + " mit Käse";
+    public override decimal Preis => _compoment.Preis + 2m;
+}
---
+++ HalloDecorator/HalloDecorator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var p1 =  new Käse(new Käse(new Käse( new Pizza())));
+var b1 =  new Käse(new Salami(new Käse( new Brot())));
+
+Console.WriteLine($"{p1.Name} {p1.Preis}");
+Console.WriteLine($"{b1.Name} {b1.Preis}");
+
+Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(b1));
