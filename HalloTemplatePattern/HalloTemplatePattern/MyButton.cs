ec12846a0dbed0abf0887d0392786031bb882e20
André <AndreR@ppedv.de>
2024-10-15 14:19:16 +0200

    Add new Windows Forms app with custom button
    Introduced a new Visual Studio solution and project `HalloTemplatePattern` for a Windows Forms application. Key additions include:
    
    - `HalloTemplatePattern.sln`: Solution file including the project.
    - `HalloTemplatePattern.csproj`: Project file targeting .NET 8.0 for Windows.
    - `Form1.Designer.cs`: Designer file for `Form1` with a standard and custom button.
    - `Form1.cs`: Code-behind file initializing `Form1`.
    - `Form1.resx`: Resource file for `Form1`.
    - `MyButton.cs`: Custom button class `MyButton` with unique painting logic.
    - `Program.cs`: Entry point running `Form1`.

---
+++ HalloTemplatePattern/HalloTemplatePattern/MyButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloTemplatePattern
+{
+    internal class MyButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+
+            pevent.Graphics.FillRectangle(new SolidBrush( Parent.BackColor), 0, 0, Width, Height);
+            pevent.Graphics.FillEllipse(Brushes.Red, 0, 0, Width, Height);
+            pevent.Graphics.DrawString(Text, Font, Brushes.White, 0, 0);
+            //base.OnPaint(pevent);
+            //Text=$"MyButton: {count++}";
+        }
+        int count = 0;
+    }
+}
