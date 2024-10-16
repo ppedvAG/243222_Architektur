64ecbd44e258ff4f4e7cdd94b4c28d6ffdc46ddb
André <AndreR@ppedv.de>
2024-10-16 15:27:09 +0200

    Add new BeanRider.UI.Web.MVC project and initial setup
    * Added a new project `BeanRider.UI.Web.MVC` to the solution file `BeanRider.sln`.
    * Updated the solution configuration to include build configurations for the new project.
    * Added `appsettings.Development.json` and `appsettings.json` configuration files with logging settings.
    * Created `BeanRider.UI.Web.MVC.csproj` targeting .NET 8.0 with necessary references.
    * Added `Program.cs` to set up the ASP.NET Core application, including service registration and middleware configuration.
    * Added `DrinkController.cs` for CRUD operations on `Drink` entities using an `IRepository`.
    * Added `HomeController.cs` for basic home and error pages.
    * Added `ErrorViewModel.cs` to represent error information.
    * Added `launchSettings.json` for environment-specific launch settings.
    * Added `_ViewImports.cshtml` and `_ViewStart.cshtml` for Razor views.
    * Added Razor views for `Drink` entity operations and basic home pages.
    * Added `_Layout.cshtml` for the main layout, `_Layout.cshtml.css` for custom styles, and `_ValidationScriptsPartial.cshtml` for client-side validation scripts.
    * Updated `favicon.ico` with new content.
    * Modified `site.css` for improved responsiveness and accessibility.
    * Created `site.js` for future custom JavaScript code.
    * Added `LICENSE` file with the MIT License text.

---
+++ BeanRider/BeanRider.UI.Web.MVC/Controllers/DrinkController.cs
@@ -0,0 +1,100 @@
+﻿using BeanRider.Model.Contracts;
+using BeanRider.Model.DomainModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeanRider.UI.Web.MVC.Controllers
+{
+    public class DrinkController : Controller
+    {
+        IRepository repo;
+
+        public DrinkController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: DrinkController
+        public ActionResult Index()
+        {
+            return View(repo.GetAll<Drink>());
+        }
+
+        // GET: DrinkController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repo.GetById<Drink>(id));
+        }
+
+        // GET: DrinkController/Create
+        public ActionResult Create()
+        {
+            return View(new Drink() { Name = "Neu", Price = 4 });
+        }
+
+        // POST: DrinkController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Drink drink)
+        {
+            try
+            {
+                repo.Add(drink);
+                repo.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DrinkController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(repo.GetById<Drink>(id));
+        }
+
+        // POST: DrinkController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Drink drink)
+        {
+            try
+            {
+                repo.Update(drink);
+                repo.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DrinkController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(repo.GetById<Drink>(id));
+        }
+
+        // POST: DrinkController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Drink drink)
+        {
+            try
+            {
+                repo.Delete(drink);
+                repo.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BeanRider/BeanRider.UI.Web.MVC/Program.cs
@@ -0,0 +1,33 @@
+using BeanRider.Data.Db;
+using BeanRider.Model.Contracts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=BeanRider_Tests;Trusted_Connection=True;";
+builder.Services.AddScoped<IRepository>(x => new EfContextRepositoryAdapter(conString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
