49afee5c263b941f2525abf29db0df384e6f7c07
André <AndreR@ppedv.de>
2024-10-16 11:46:30 +0200

    Add BeanRider.Logic and tests for OrderService
    Added new projects `BeanRider.Logic` and `BeanRider.Logic.Tests` to the solution. Updated solution configuration to include build configurations for these projects. Implemented `OrderService` class with `CalculateTotalPrice` method in `BeanRider.Logic`. Created `OrderServiceTests` in `BeanRider.Logic.Tests` to test `OrderService`, including tests for null orders, empty orders, and orders with items. Added necessary package and project references for xUnit, FluentAssertions, and `BeanRider.Model`.

---
+++ BeanRider/BeanRider.Logic.Tests/OrderServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using BeanRider.Model;
+using FluentAssertions;
+
+namespace BeanRider.Logic.Tests
+{
+    public class OrderServiceTests
+    {
+        [Fact]
+        public void CalculateTotalPrice_should_throw_ArgumentEx_if_order_is_null()
+        {
+            var orderService = new OrderService();
+
+            Action act = () => orderService.CalculateTotalPrice(null!);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void CalculateTotalPrice_should_return_0_if_order_has_no_items()
+        {
+            var orderService = new OrderService();
+            var order = new Order() { Customer = new Customer() { Name = "Fred", Number = "4" } };
+            var totalPrice = orderService.CalculateTotalPrice(order);
+            totalPrice.Should().Be(0);
+        }
+
+        [Fact]
+        public void CalculateTotalPrice_ShouldReturnCorrectTotalPrice()
+        {
+            // Arrange
+            var orderService = new OrderService();
+            var order = new Order
+            {
+                Customer = new Customer() { Name = "Fred", Number = "4" }
+            };
+            order.Items = new List<OrderItem>
+                    {
+
+                        new OrderItem
+                        {
+                            Food = new Drink { Price = 10,Name="Coffee" },
+                            Amount = 2,
+                            Order = order
+                        },
+                        new OrderItem
+                        {
+                            Food = new Eatable { Price = 5,Name="Cake" },
+                            Amount = 3,
+                            OrderPrice = 4,
+                            Order = order
+                        }
+                    };
+
+            // Act
+            decimal totalPrice = orderService.CalculateTotalPrice(order);
+
+            // Assert
+            totalPrice.Should().Be(32);
+        }
+
+    }
+}
---
+++ BeanRider/BeanRider.Logic/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using BeanRider.Model;
+
+namespace BeanRider.Logic
+{
+    public class OrderService
+    {
+
+        public decimal CalculateTotalPrice(Order order)
+        {
+            ArgumentNullException.ThrowIfNull(order, nameof(order));
+
+            decimal totalPrice = 0;
+            foreach (var item in order.Items)
+            {
+                decimal itemPrice = item.Food.Price;
+                if (item.OrderPrice.HasValue)
+                    itemPrice = item.OrderPrice.Value;
+
+                //todo
+                //if (item.Discount.HasValue)
+                //    itemPrice *= item.Discount.Value * -1;
+
+                totalPrice += itemPrice * item.Amount;
+            }
+            return totalPrice;
+        }
+
+    }
+}
