8e224cc91a264a4ac16bd6bab3c35b7b7922165a
André <AndreR@ppedv.de>
2024-10-16 13:54:18 +0200

    Add BeanRider.UI.DevConsole project to solution
    A new project named `BeanRider.UI.DevConsole` has been added to the `BeanRider.sln` solution. The project is defined in `BeanRider.UI.DevConsole\BeanRider.UI.DevConsole.csproj` with the unique identifier `{40636B65-D8F4-4A7E-B4D6-F6C38BAD90B6}`.
    
    The solution configuration has been updated to include build configurations for `Debug|Any CPU` and `Release|Any CPU` for the new project.
    
    The `BeanRider.UI.DevConsole.csproj` file has been created with the following properties:
    - Uses `Microsoft.NET.Sdk`
    - Output type set to `Exe`
    - Target framework set to `net8.0`
    - Implicit usings and nullable reference types enabled
    - References to `BeanRider.Data.Db`, `BeanRider.Logic`, and `BeanRider.Model`
    
    A new `Program.cs` file has been added to the `BeanRider.UI.DevConsole` project, which includes:
    - Using directives for `BeanRider.Data.Db`, `BeanRider.Logic`, `BeanRider.Model.Contracts`, and `BeanRider.Model.DomainModel`
    - Code to set console output encoding to UTF-8
    - A welcome message for the application
    - Initialization of a connection string for a local database
    - Creation of a repository and an order service
    - Code to display all drinks and their prices from the repository
    - Code to display all open orders that are not vegetarian, including details of each order item

---
+++ BeanRider/BeanRider.UI.DevConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using BeanRider.Data.Db;
+using BeanRider.Logic;
+using BeanRider.Model.Contracts;
+using BeanRider.Model.DomainModel;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("*** BeanRider v0.1 PREMIUM EDITION ***");
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=BeanRider_Tests;Trusted_Connection=True;";
+
+IRepository repo = new EfContextRepositoryAdapter(conString);
+OrderService orderService = new OrderService(repo);
+
+foreach (var item in repo.GetAll<Drink>())
+{
+    Console.WriteLine($"{item.Name} - {item.Price}");
+}
+
+foreach (var item in orderService.GetOpenOrdersThatAreNotVegetarian())
+{
+    Console.WriteLine($"{item.Time} - {item.Status}");
+    foreach (var orderItem in item.Items)
+    {
+        Console.WriteLine($"\t{orderItem.Food.Name} - {orderItem.Amount} {orderItem.OrderPrice:c} ");
+    }
+}
