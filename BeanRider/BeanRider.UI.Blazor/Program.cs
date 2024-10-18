720057af18c54e8f0e7f3d97de63c501375406d8
André <AndreR@ppedv.de>
2024-10-18 15:49:59 +0200

    Add new Blazor UI and Azure Function projects
    Added a new Blazor UI project `BeanRider.UI.Blazor` to the solution `BeanRider.sln` with necessary configurations, components, and styles. Updated the solution to include build configurations for the new project. Added various Razor components and CSS rules for the Blazor application.
    
    Created a new Azure Function project `FunctionApp1` with a sample HTTP-triggered function, logging configurations, and dependencies. Added necessary project files, including `.gitignore`, `host.json`, and `launchSettings.json`.
    
    Added a new Visual Studio solution `OpenAPIClient.sln` for the OpenAPIClient project, including dependencies and a sample program to demonstrate usage of the generated OpenAPI client.

---
+++ BeanRider/BeanRider.UI.Blazor/Program.cs
@@ -0,0 +1,35 @@
+using BeanRider.Data.Db;
+using BeanRider.Logic;
+using BeanRider.Model.Contracts.Data;
+using BeanRider.UI.Blazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=BeanRider_Tests;Trusted_Connection=True;";
+builder.Services.AddScoped<IRepository>(x => new EfContextRepositoryAdapter(conString));
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ OpenAPIClient/OpenAPIClient/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var client = new OpenAPIClient.swaggerClient("https://localhost:7116",new HttpClient());
+
+var orders = await client.OrdersAllAsync();
+
+foreach (var item in orders)
+{
+    Console.WriteLine(item.CustomerName);
+}
